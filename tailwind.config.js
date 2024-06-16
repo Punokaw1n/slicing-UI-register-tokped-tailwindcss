9d534ac30f75df9ad5d8745ba75624ed7a6b36cc
Fery Yulia Rahman <feryyuliarahman11@gmail.com>
2024-06-16 16:47:18 +0700

    slicing UI registrasi tokped tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#42B549",
+        secondary: "#00AA5B",
+      },
+    },
+  },
+  plugins: [],
+};
